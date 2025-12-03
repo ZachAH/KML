38c715dd1d195f22b8ae0f864cfa6bd97282da01
zach <etta1055@gmail.com>
2025-12-03 17:54:13 -0600

    hero update

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,7 @@
 import "./LandingPage.css";
 import Services from "./Services";
-import heroVideo from "../assets/cleaning/carpet_cleaning.webm";
-import heroMobileTablet from "../assets/cleaning/KML_MOBILE_TABLET.webm";
-import heroMobilePhone from "../assets/cleaning/KML_LOGO_PHONE.mp4";
+
+import heroVideo from "../assets/cleaning/landing_page.mp4";
 
 
 const LandingPage: React.FC = () => {
@@ -11,6 +10,8 @@ const LandingPage: React.FC = () => {
 
       {/* Hero Section */}
       <section className="hero">
+
+        {/* Hero Video */}
         <video
           className="hero-video"
           autoPlay
@@ -19,30 +20,8 @@ const LandingPage: React.FC = () => {
           playsInline
           preload="metadata"
         >
-          <source src={heroVideo} type="video/mp4" />
+          <source src={heroVideo} type="video/webm" />
         </video>
-        {/* Tablet video */}
-        <video
-          className="hero-video tablet-video"
-          autoPlay
-          loop
-          muted
-          playsInline
-        >
-          <source src={heroMobileTablet} type="video/webm" />
-        </video>
-
-        {/* Small phone video */}
-        <video
-          className="hero-video phone-video"
-          autoPlay
-          loop
-          muted
-          playsInline
-        >
-          <source src={heroMobilePhone} type="video/webm" />
-        </video>
-
 
         {/* Hero Content */}
         <div className="hero-content">
